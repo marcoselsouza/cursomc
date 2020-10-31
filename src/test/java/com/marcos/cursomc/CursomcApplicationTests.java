f8710224abbd6c581c845fb5438c0efa6f4c38f3
Marcoselsouza <marcoselsouza@yahoo.com.br>
2020-10-31 17:31:32 -0300

    projeto criado

---
+++ src/test/java/com/marcos/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
